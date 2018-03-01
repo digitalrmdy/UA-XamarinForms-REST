ef3fa5a2c28cae9f8a0bbb753eefa09297b293d8
Samuel Debruyn <s@muel.be>
2018-03-01 15:13:04 +0100

    content of stylespage

--- FindMyBeer/Views/StylesPage.xaml.cs
+++ FindMyBeer/Views/StylesPage.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using FindMyBeer.ViewModels;
 using Xamarin.Forms;
+using Style = FindMyBeer.Models.Style;
 
 namespace FindMyBeer.Views
 {
@@ -10,6 +11,28 @@ namespace FindMyBeer.Views
 		public StylesPage()
 		{
 			InitializeComponent();
+			BindingContext = new StylesViewModel();
+		}
+
+		void ListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			if (!(e.SelectedItem is Style style))
+			{
+				return;
+			}
+
+			((ListView)sender).SelectedItem = null;
+			// TODO
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			StylesViewModel viewModel = BindingContext as StylesViewModel;
+			if (viewModel != null && viewModel.Styles.Count == 0)
+			{
+				viewModel.ReloadStylesCommand.Execute(null);
+			}
 		}
 	}
 }
