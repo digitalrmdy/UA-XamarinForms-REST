7000177983ee5c62898966733f22a58a108ed9e1
Samuel Debruyn <s@muel.be>
2018-03-01 13:48:15 +0100

    create new project

---
+++ FindMyBeer/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FindMyBeer
+{
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
