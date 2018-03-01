f8c0cf0e3614ce6fbfe5e735a969f20b2cf895bc
Samuel Debruyn <s@muel.be>
2018-03-01 15:04:26 +0100

    add stylesviewmodel

---
+++ FindMyBeer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FindMyBeer.Services;
+using Refit;
+using Xamarin.Forms;
+using PropertyChanged;
+
+namespace FindMyBeer
+{
+	[AddINotifyPropertyChangedInterface]
+	public class BaseViewModel
+	{		
+		protected IBeerApi ApiService { get; }
+		public bool IsLoading { get; set; }
+
+		public BaseViewModel()
+		{
+			ApiService = RestService.For<IBeerApi>(Constants.BASE_URL);
+		}
+	}
+}
---
+++ FindMyBeer/ViewModels/StylesViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FindMyBeer.Extensions;
+using FindMyBeer.Models;
+using Xamarin.Forms;
+using Style = FindMyBeer.Models.Style;
+
+namespace FindMyBeer.ViewModels
+{
+	public class StylesViewModel: BaseViewModel
+	{
+		public ObservableCollection<Style> Styles { get; }
+		public ICommand ReloadStylesCommand { get; }
+		
+		public StylesViewModel()
+		{
+			Styles = new ObservableCollection<Style>();
+			ReloadStylesCommand = new Command(async () => await ReloadStyles());
+		}
+
+		async Task ReloadStyles()
+		{
+			Styles.Clear();
+			
+			EntityResponse<List<Style>> resultFromApi;
+			try
+			{
+				resultFromApi = await ApiService.GetStyles().SetIsLoading(this);
+			}
+			catch (Exception)
+			{
+				return;
+			}
+			
+			if (resultFromApi.Status == Status.Failure)
+			{
+				return;
+			}
+			
+			resultFromApi.Data.ForEach(s => Styles.Add(s));
+		}
+	}
+}
